b49a73603eb7e9072db89a2b256d1a49fe5da15a
dwctien <kductien712@gmail.com>
2024-11-30 22:45:09 +0700

    Update myvector.h

--- 2048/myvector.h
+++ 2048/myvector.h
@@ -3,7 +3,7 @@
 
 #include "system.h"
 
-struct VectorNode {							// mỗi phần tử (node) của vector gồm một cặp số (x,y)
+struct VectorNode {					// mỗi phần tử (node) của vector gồm một cặp số (x,y)
 	int x, y;
 	VectorNode* next;
 };
@@ -17,7 +17,7 @@ struct myVector {
 
 void initVector(myVector& v);				// khởi tạo vector rỗng
 
-void v_push(myVector& v, int x, int y);		// thêm phần tử mới vào vector
+void v_push(myVector& v, int x, int y);			// thêm phần tử mới vào vector
 
 int v_ix(myVector& v, int index);			// lấy ra thành phần thứ nhất của phần tử có chỉ số index trong vector 
 
